bd9bc4a7f435987ed87d4c1b8351180023fa8a5c
andrew <andrew@example.com>
2020-04-25 10:49:48 +0800

    Initial version of gateway generated by JHipster-6.8.0

---
+++ gateway/src/main/java/com/jhipster/demo/gateway/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.jhipster.demo.gateway.config.audit;
---
+++ gateway/src/main/java/com/jhipster/demo/gateway/service/InvalidPasswordException.java
@@ -0,0 +1,11 @@
+package com.jhipster.demo.gateway.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+
+}
